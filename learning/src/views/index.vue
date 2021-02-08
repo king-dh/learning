bba8450825afe06890c8079696eb7b3388348b0b
dengh <1660773583@qq.com>
2021-02-08 14:07:28 +0800

    合并代码

--- learning/src/views/index.vue
+++ learning/src/views/index.vue
@@ -1,11 +1,7 @@
 <template>
 <div>
     <div>欢迎</div>
-<<<<<<< HEAD
-    <button>Login<button>
-=======
     <button>Login</button>
     <button>feature-1.1-deng</button>
->>>>>>> feb95a0... 添加按钮
 </div>
 </template>
