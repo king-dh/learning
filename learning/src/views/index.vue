7f123237d0d45b90d986cb930282f7b1ae949610
dengh <1660773583@qq.com>
2021-02-08 15:33:04 +0800

    测试自定义组件用法

--- learning/src/views/index.vue
+++ learning/src/views/index.vue
@@ -2,6 +2,18 @@
 <div>
     <div>欢迎</div>
     <button>Login<button>
-    <button>feature-1.1-deng</button>
+    <button @click="routerHandle">Login</button>
 </div>
 </template>
+<script>
+export default {
+    data () {
+        return {}
+    },
+    methods: {
+        routerHandle () {
+            this.$router.push('/activity/index')
+        }
+    }
+}
+</script>
